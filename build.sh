528d5bd51df7e7991595961ef593e29620adfefd
Niklas Holmstrand <niklas.holmstrand@gmail.com>
2020-03-07 10:25:51 +0100

    Add protocompile cmd

---
+++ build.sh
@@ -0,0 +1,2 @@
+~/protoc/protoc-3.11.4-linux-x86_64/bin/protoc --js_out=import_style=commonjs,binary:. tpcp0.proto
+~/protoc/protoc-3.11.4-linux-x86_64/bin/protoc --js_out=import_style=commonjs,binary:. resource_mgr.proto
